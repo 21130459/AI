ea6d04b9725b7ebc9ab66afd3344ea6993bd7e3f
HuuNhan <nhan0925195109@gmail.com>
2023-11-13 21:00:20 +0700

    lab6

---
+++ AI_Lab6_InformedSearch_8_Queen/src/classes/Test.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Test {
+	public static void main(String[] args) {
+		Node initialState = new Node();
+        initialState.generateBoard();
+
+        System.out.println("Initial State:");
+        initialState.displayBoard();
+        System.out.println("Initial Heuristic: " + initialState.getH());
+
+        Node finalState = initialState.executeHillClimbingWithRandomRestart(initialState, 100);
+
+        System.out.println("\nFinal State:");
+        finalState.displayBoard();
+        System.out.println("Final Heuristic: " + finalState.getH());
+	}
+}
